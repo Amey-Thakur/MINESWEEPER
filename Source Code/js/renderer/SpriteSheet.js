d11bd561e83a069f557230f47b267dd2ab104ea9
Amey Thakur <ameythakur20@gmail.com>
2026-03-08 04:57:50 -0400

    Minesweeper

--- Source Code/js/renderer/SpriteSheet.js
+++ Source Code/js/renderer/SpriteSheet.js
@@ -47,25 +47,25 @@ export class SpriteSheet {
 
         this.colors = [
             null,
-            '#0000FF', // 1: Blue
-            '#008000', // 2: Green
-            '#FF0000', // 3: Red
-            '#000080', // 4: Navy
-            '#800000', // 5: Maroon
-            '#008080', // 6: Teal
-            '#000000', // 7: Black
-            '#808080'  // 8: Gray
+            '#0000FF',  // 1: Blue
+            '#008000',  // 2: Green
+            '#FF0000',  // 3: Red
+            '#000080',  // 4: Navy
+            '#FF69B4',  // 5: Pink 
+            '#00FFFF',  // 6: Cyan
+            '#000000',  // 7: Black
+            '#808080'   // 8: Gray
         ];
 
         this.numberPaths = {
-            1: "M 40 120 V 0 H 60 V 120 Z",
-            2: "M 10 0 H 90 V 20 H 30 V 50 H 90 V 120 H 10 V 100 H 70 V 70 H 10 Z",
-            3: "M 10 0 H 90 V 120 H 10 V 100 H 70 V 70 H 30 V 50 H 70 V 20 H 10 Z",
-            4: "M 70 120 H 90 V 0 H 70 L 10 60 V 80 H 70 Z M 70 20 V 60 H 30 Z",
-            5: "M 90 0 H 10 V 70 H 90 V 120 H 10 V 100 H 70 V 50 H 30 V 20 H 90 Z",
-            6: "M 10 0 H 90 V 20 H 30 V 50 H 90 V 120 H 10 Z M 30 70 V 100 H 70 V 70 Z",
-            7: "M 10 0 H 90 L 50 120 H 30 L 70 20 H 10 Z",
-            8: "M 10 0 H 90 V 120 H 10 Z M 30 20 V 50 H 70 V 20 Z M 30 70 V 100 H 70 V 70 Z"
+            1: "M0 105.758v-11.75H23.502V47.003H0V35.253H11.751V23.502H23.502V11.75H35.253V0H58.755v94.007H82.256v23.502H0Z",
+            2: "M0 99.883V82.256H11.751V70.505H35.253v-11.75H58.755V47.004H82.256V23.502H35.253V35.253H0V11.75H11.751V0h94.007V11.751H117.51v35.253H105.758V58.755H94.007v11.75H70.505V82.256H47.004V94.007h70.505v23.502H0Z",
+            3: "M0 105.758v-11.75h82.256V70.505H35.253V47.003h47.003V23.502H0V0h105.759V11.751h11.75v35.253H105.758V70.505H117.51v35.253H105.758V117.51H0Z",
+            4: "M70.505 94.007V70.505H0V47.004H11.751V23.502H23.502V0h35.253V23.502H47.004V47.004H70.505V0h35.253v47.004H117.51V70.505H105.758v47.004H70.505Z",
+            5: "M0 105.758v-11.75h82.256V70.505H0V0h117.509V23.502H35.253V47.004h70.505V58.755H117.51v47.003H105.758V117.51H0Z",
+            6: "M11.75 111.634v-5.876H0V11.751H11.751V0h94.007V23.502H35.253V47.004h70.505V58.755H117.51v47.003H105.758V117.51H11.751Zm70.506-29.378v-11.75H35.253V94.006h47.003z",
+            7: "M47.004 105.758v-11.75H58.755V70.505h11.75V47.003H82.256V23.502H0V0h117.509v47.004H105.758V70.505H94.007v23.502H82.256v23.502H47.004Z",
+            8: "M11.75 111.634v-5.876H0V70.505H11.751V47.004H0V11.75H11.751V0h94.007V11.751H117.51v35.253H105.758V70.505H117.51v35.253H105.758V117.51H11.751zm70.506-29.378v-11.75H35.253V94.006h47.003zm0-47.003V23.502H35.253V47.004h47.003z"
         };
 
         this.generateSprites();
@@ -80,8 +80,8 @@ export class SpriteSheet {
         // 1: Empty (Sunken/Flat)
         this.drawFlat(1 * CELL_SIZE);
 
-        // 2: Mine
-        this.drawFlat(2 * CELL_SIZE);
+        // 2: Mine (All revealed mines red bg as requested)
+        this.drawFlat(2 * CELL_SIZE, '#ff0000');
         this.drawMine(2 * CELL_SIZE);
 
         // 3: Flag
@@ -92,9 +92,10 @@ export class SpriteSheet {
         this.drawBevel(4 * CELL_SIZE, true);
         this.drawText(4 * CELL_SIZE, '?', '#000000');
 
-        // 5: Red Mine (The one the user clicked)
+        // 5: Red Mine (The one that exploded - with cross as requested)
         this.drawFlat(5 * CELL_SIZE, '#ff0000');
         this.drawMine(5 * CELL_SIZE);
+        this.drawCross(5 * CELL_SIZE);
 
         // 6: Crossed Mine (Wrongly flagged)
         this.drawFlat(6 * CELL_SIZE);
@@ -145,18 +146,12 @@ export class SpriteSheet {
         const pathData = this.numberPaths[num];
         if (!pathData) return;
 
-        // These paths are built on a 100x120 grid
         const vW = 100;
         const vH = 120;
 
-        const targetSize = CELL_SIZE * 0.70;
-        const scale = targetSize / vH;
-
-        const drawW = vW * scale;
-        const drawH = vH * scale;
-
-        const offsetX = x + (CELL_SIZE - drawW) / 2;
-        const offsetY = (CELL_SIZE - drawH) / 2;
+        const scale = (CELL_SIZE * 0.70) / vH; // 70% of cell height
+        const offsetX = x + (CELL_SIZE - vW * scale) / 2;
+        const offsetY = (CELL_SIZE - vH * scale) / 2;
 
         ctx.save();
         ctx.translate(offsetX, offsetY);
@@ -168,58 +163,63 @@ export class SpriteSheet {
 
     drawMine(x) {
         const ctx = this.ctx;
-        const scale = (CELL_SIZE * 0.70) / 100;
-        const offset = (CELL_SIZE - 100 * scale) / 2;
+        const scale = (CELL_SIZE * 0.70) / 117.509; // Match number scale
+        const offset = (CELL_SIZE - 117.509 * scale) / 2;
 
         ctx.save();
         ctx.translate(x + offset, offset);
         ctx.scale(scale, scale);
 
-        // White Highlight
+        // White Highlight Square
         ctx.fillStyle = "#fff";
-        ctx.fillRect(25, 25, 20, 20);
+        ctx.fill(new Path2D("M35.134 34.736h20.478v20.612H35.134z"));
 
         // Black Mine Body
         ctx.fillStyle = "#000";
-        const body = new Path2D("M50 0h10v20H50V0z M50 80h10v20H50V80z M0 50h20v10H0V50z M80 50h20v10H80V50z M15 15h15v15H15V15z M70 15h15v15H70V15z M15 70h15v15H15V70z M70 70h15v15H70V70z M20 30h60v40H20V30z M30 20h40v60H30V20z");
-        ctx.fill(body);
+        ctx.fill(new Path2D("M54.29 108.46v-9.047H36.192v-11.751h-9.048v11.751h-9.049v-11.751h9.049v-9.048h-9.049V63.22H0V54.289h18.096V36.193h9.049v-9.048h-9.049v-9.049h9.049v9.049h9.048v-9.049h18.096V0H63.22v18.096h18.096v9.049h9.048v-9.049h9.049v9.049h-9.049v9.048h9.049v18.096h18.096V63.22H99.413v18.096h-9.049v9.048h9.049v9.049h-9.049v-9.049h-9.048v9.049H63.22v18.096H54.289zm0-63.219v-9.048H36.192v18.096h18.096z"));
 
         ctx.restore();
     }
 
     drawFlag(x) {
         const ctx = this.ctx;
-        const scale = (CELL_SIZE * 0.70) / 100;
-        const offset = (CELL_SIZE - 100 * scale) / 2;
-
         ctx.save();
-        ctx.translate(x + offset, offset);
-        ctx.scale(scale, scale);
+        ctx.translate(x, 0);
 
-        // Flag body (Vibrant RED triangle - pointing LEFT)
+        // Transform is matrix(.44413 0 0 .44413 86.605 122.579)
+        // SVG size is 355x444. Scale it to fit CELL_SIZE (24x24) or similar.
+        // The original SpriteSheet used scale = (CELL_SIZE * 0.70) / 100.
+        // Let's use a scale that fits the 117.509 viewBox used for numbers to stay consistent if possible, 
+        // but flag.svg has a different viewBox. Let's just use the matrix from the SVG.
+
+        ctx.translate(CELL_SIZE * 0.15, CELL_SIZE * 0.15); // Center padding
+        const svgScale = (CELL_SIZE * 0.7) / 444;
+        ctx.scale(svgScale, svgScale);
+
+        // Path 1 (Red Flag)
+        ctx.save();
+        ctx.transform(0.44413, 0, 0, 0.44413, 86.605, 122.579);
         ctx.fillStyle = "red";
-        ctx.fill(new Path2D("M55 10 L15 35 L55 60 Z"));
+        ctx.fill(new Path2D("M105 174v-50H-95V24h-100V-76h100v-100h200v-100h200v500H105Z"));
+        ctx.restore();
 
-        // Classic pole and stepped base (Solid BLACK)
+        // Path 2 (Black Pole)
+        ctx.save();
+        ctx.transform(0.44413, 0, 0, 0.44413, 86.605, 122.579);
         ctx.fillStyle = "#000";
-        ctx.fill(new Path2D("M55 10 h10 v70 h-10 Z M35 80 h50 v10 h-50 Z M20 90 h80 v10 h-80 Z"));
+        ctx.fill(new Path2D("M-195 624V524H5V424h200V224h100v200h100v100h200v200h-800Z"));
+        ctx.restore();
 
         ctx.restore();
     }
 
     drawCross(x) {
         const ctx = this.ctx;
-        const vW = 117.509;
-        const vH = 117.509;
-
-        const targetSize = CELL_SIZE * 0.75;
-        const scale = targetSize / vH;
-
-        const offsetX = x + (CELL_SIZE - targetSize) / 2;
-        const offsetY = (CELL_SIZE - targetSize) / 2;
+        const scale = (CELL_SIZE * 0.70) / 117.509;
+        const offset = (CELL_SIZE - 117.509 * scale) / 2;
 
         ctx.save();
-        ctx.translate(offsetX, offsetY);
+        ctx.translate(x + offset, offset);
         ctx.scale(scale, scale);
 
         ctx.fillStyle = "red";
