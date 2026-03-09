7c7cb7c3377ec774763deceda693667716fb21c6
Amey Thakur <ameythakur20@gmail.com>
2026-03-08 23:59:10 -0400

    Minesweeper

--- Source Code/sw.js
+++ Source Code/sw.js
@@ -17,7 +17,7 @@
  * Space Complexity          : O(M) for local persistence of M total asset bytes.
  */
 
-const CACHE_NAME = 'minesweeper-pwa-v10';
+const CACHE_NAME = 'minesweeper-pwa-v12';
 const ASSETS = [
     './',
     './index.html',
